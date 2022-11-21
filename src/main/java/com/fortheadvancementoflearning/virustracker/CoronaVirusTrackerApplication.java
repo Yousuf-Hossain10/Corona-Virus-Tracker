75bdcbe5674498678970b2a7bca0f57ccd014d5c
Yousuf Hossain Anik <mdyousufhossainanik10@gmail.com>
2022-11-21 23:48:38 +0600

    first commit

---
+++ src/main/java/com/fortheadvancementoflearning/virustracker/CoronaVirusTrackerApplication.java
@@ -0,0 +1,16 @@
+package com.fortheadvancementoflearning.virustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronaVirusTrackerApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaVirusTrackerApplication.class, args);
+	}
+
+}
